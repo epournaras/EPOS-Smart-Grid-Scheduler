21e21252a56f0e70b1e7b0db578e1a406596aa65
STSWarren <stuartwarren1995@gmail.com>
2016-12-19 13:22:29 +0100

    Update ScheduleApp.java
    Changed how testing was performed to easily allow changing the amount of inputs more easily, and that inputs would be randomly generated to see if any cases break the logic.

--- Current-Code/ScheduleApp.java
+++ Current-Code/ScheduleApp.java
@@ -1,18 +1,83 @@
 public class ScheduleApp {
 	public static void main(String[] args){		
-		Action testOne = new Action("One", "08:30","11:15","00:45", 0.65);
 		
-		Action testTwo = new Action("Two", "13:05" ,"16:05","01:00", 0.5);
-		
-		Action testThree = new Action("Three","09:45","13:30","00:15" , 0.5);
-		
-		Action testFour = new Action("Four", "15:05","15:30","00:25" , 0.5);
-		
-		Action[] list = {testOne,testTwo,testThree, testFour};
-		
-		Schedule testSchedule = new Schedule(list);
+		Schedule testSchedule = new Schedule();
+
+		int n = 7;
+		for(int i = 0; i < n; i++){
+		  int timeOne = 0 + (int)(Math.random() * ((1199 - 0) + 1));	
+		  int timeTwo = 0 + (int)(Math.random() * ((1340 - 0) + 1));
+		  int timeThree = 15 + (int)(Math.random() * ((120 - 15) + 1));
+		  int minTime;
+		  int maxTime;
+		  if(timeOne>timeTwo){	
+		  		minTime = timeTwo;
+		  		maxTime = timeOne;
+		  }else{
+		  		minTime = timeOne;
+		  		maxTime = timeTwo;
+		  }
+		  if(maxTime-minTime<timeThree){
+		  		maxTime = minTime+timeThree;
+		  }
+		  String maxTimeString;
+		  String minTimeString;
+		  String durationString;
+		  int hour = maxTime/60;
+		  int minute = maxTime%60;
+		  if(hour<10){
+		  	if(minute<10){
+		  			maxTimeString = "0"+hour+":"+"0"+minute;
+		 	}else{
+		  			maxTimeString = "0"+hour+":"+minute;
+		  	}
+		  }else{
+		  	if(minute<10){
+		  			maxTimeString = hour+":"+"0"+minute;
+		  	}else{
+		  			maxTimeString = hour+":"+minute;
+		  	}
+		  }
+		  hour = minTime/60;
+		  minute = minTime%60;
+		  if(hour<10){
+		  	if(minute<10){
+		  			minTimeString = "0"+hour+":"+"0"+minute;
+		 	}else{
+		 			minTimeString = "0"+hour+":"+minute;
+		 	}
+		  }else{
+		  	if(minute<10){
+		  			minTimeString = hour+":"+"0"+minute;
+		  	}else{
+		  			minTimeString = hour+":"+minute;
+		  	}
+		  }
+		  hour = timeThree/60;
+		  minute = timeThree%60;
+		  if(hour<10){
+		  	if(minute<10){
+		  			durationString = "0"+hour+":"+"0"+minute;
+		 	}else{
+		 			durationString = "0"+hour+":"+minute;
+		 	}
+		  }else{
+		  	if(minute<10){
+		  			durationString = hour+":"+"0"+minute;
+		  	}else{
+		  			durationString = hour+":"+minute;
+		  	}
+		  }
+		  System.out.print(minTimeString+" "+maxTimeString+" "+durationString+"\n");
+		  Action temp = new Action("Action "+i, minTimeString, maxTimeString, durationString, 0);
+		  testSchedule.add(cloneAction(temp));
+		}
 		
 		testSchedule.initialiseActionList();
 		testSchedule.makeScheduleList();
+	}
+	public static Action cloneAction(Action a){
+		Action temp = new Action(a);
+		return temp;
 	}
 }
