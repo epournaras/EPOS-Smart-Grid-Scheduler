c6475bb70c1f7c7b088b0e4e1262e0cc52db07ce
STSWarren <stuartwarren1995@gmail.com>
2016-12-20 14:20:07 +0100

    Update MainActivity.java
    Changed the displayMaxTime so that the the times getting added were converted to the time string equivalent of the int instead of adding the int. The same change was made to displayMinTime assignment statement. Also changed addItem as removing an unnecessary opening of a file.

--- Android-Code/MainActivity.java
+++ Android-Code/MainActivity.java
@@ -2,31 +2,28 @@ package com.example.scheduleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
 import com.example.schedulelibrary.Action;
 import com.example.schedulelibrary.Schedule;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
 
     public String selectedActiv;
     String[] actionNames = {"cooking", "vacuuming", "charging car", "showering"};
     String[] actionDurations = {"00:40", "00:25", "01:20", "00:15"};
-    Schedule lists = new Schedule();
-
+    ArrayList<Action> list = new ArrayList<Action>();
+    Schedule lists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,13 +70,13 @@ public class MainActivity extends AppCompatActivity {
     //adds an item to the schedule list.
     public void addItem(View v) {
         try {
+            //Log.d("LENGTH",""+list.size()+"\n");
             Spinner activDrp = (Spinner) findViewById(R.id.spActiv);
             selectedActiv = activDrp.getSelectedItem().toString();
-            FileInputStream fisGetFiles = openFileInput("firstAction.txt");
 
             //Get the last entered time window.
             StringBuilder builder = new StringBuilder();
-            fisGetFiles = openFileInput("tempMin.txt");
+            FileInputStream fisGetFiles = openFileInput("tempMin.txt");
             int chr;
             while ((chr = fisGetFiles.read()) != -1) {
                 builder.append((char) chr);
@@ -95,14 +92,15 @@ public class MainActivity extends AppCompatActivity {
             String duration = "";
             boolean notFound = true;
             for (int i = 0; i < actionNames.length && notFound; i++) {
-                if (selectedActiv.equals(actionNames.length)) {
+                if (selectedActiv.equals(actionNames[i])) {
                     duration = actionDurations[i];
                     notFound = false;
                 }
             }
-            Action toSubmit = new Action(selectedActiv, tempMin, tempMax, duration, 0);
-            lists.add(toSubmit);
+            list.add(new Action(selectedActiv, tempMin, tempMax, duration, 0));
+
         } catch (IOException ex) {
+            //Log.d("EXCEPTION", "We never stood a chance \n");
             ex.printStackTrace();
         }
     }
@@ -111,20 +109,25 @@ public class MainActivity extends AppCompatActivity {
     //create schedule arrangements from list, pick one, send it on to the display.
     public void sendMessage(View view) {
 
-
         try {
             String displayName = "";
             String displayMaxTime = "";
             String displayMinTime = "";
             FileOutputStream fosInput = openFileOutput("nameOfAction.txt", Context.MODE_PRIVATE);
+            list.trimToSize();
+            Action[] array = new Action[list.size()];
+            list.toArray(array);
+            lists = new Schedule(array);
+            //Log.d("LISTS",""+list.size()+"\n");
             lists.makeScheduleList();
             Action[][] fullList = lists.getFullList();
+            Log.d("LISTS",""+fullList.length+"\n");
             int i = 0;
-            if (fullList != null) {
-                for (int j = 0; j < fullList.length; j++) {
+            if (fullList.length !=0) {
+                for (int j = 0; j < fullList[i].length; j++) {
                     displayName += fullList[i][j].name + ",";
-                    displayMinTime += fullList[i][j].windowStart + ",";
-                    displayMaxTime += fullList[i][j].windowEnd + ",";
+                    displayMinTime += fullList[i][j].getTimeString(fullList[i][j].windowStart) + ",";
+                    displayMaxTime += fullList[i][j].getTimeString(fullList[i][j].windowEnd) + ",";
 
                 }
                 fosInput.write(displayName.getBytes());
@@ -132,9 +135,9 @@ public class MainActivity extends AppCompatActivity {
                 fosInput.write(displayMinTime.getBytes());
                 fosInput = openFileOutput("maxTime.txt", Context.MODE_PRIVATE);
                 fosInput.write(displayMaxTime.getBytes());
-                Intent intent = new Intent(this, DisplayMessageActivity.class);
-                startActivity(intent);
             }
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            startActivity(intent);
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
