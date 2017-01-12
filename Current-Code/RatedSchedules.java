b82d8c324e485535940bcacc5cb461c9dce8f3bc
STSWarren <stuartwarren1995@gmail.com>
2017-01-12 14:26:33 +0100

    Update RatedSchedules.java
    No longer needed in current build. Keeping just in case

--- Current-Code/RatedSchedules.java
+++ Current-Code/RatedSchedules.java
@@ -1,14 +1,16 @@
-public class RatedSchedules {
-	public double[] ratings;
-	public Action[][] schedules;
-	
-	public RatedSchedules(int i){
-		this.ratings = new double[i];
-		this.schedules = new Action[i][];
-	}
-	
-	public RatedSchedules(double[] r, Action[][]s){
-		this.ratings = r;
-		this.schedules =s;
-	}
-}
+
+//public class RatedSchedules {
+//	public double[] ratings;
+//	public Action[][] schedules;
+//	
+//	public RatedSchedules(int i){
+//		this.ratings = new double[i];
+//		this.schedules = new Action[i][];
+//	}
+//	
+//	public RatedSchedules(double[] r, Action[][]s){
+//		this.ratings = r;
+//		this.schedules =s;
+//	}
+//}
+
