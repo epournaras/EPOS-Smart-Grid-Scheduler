da9f9493db72d5377c5afaa26521fe8d42f78d41
STSWarren <stuartwarren1995@gmail.com>
2017-01-12 14:27:00 +0100

    Update ParallelSortingRegularSampling.java

--- Current-Code/ParallelSortingRegularSampling.java
+++ Current-Code/ParallelSortingRegularSampling.java
@@ -18,61 +18,41 @@ public class ParallelSortingRegularSampling {
 	/*
 	 * Quicksort
 	 */
-	public RatedSchedules sort(RatedSchedules a){
-		double[] sortedList = new double[a.ratings.length];
-		RatedSchedules concatonatedSortedSchedules;
-		if(a.ratings.length>1){
-			double[] tempHigher = new double[a.ratings.length];
-			double[] tempLower = new double[a.ratings.length];
-			double pivot = a.ratings[0];
-			Action[] pivotAction = a.schedules[0];
-			Action[][] tempHigherActions = new Action[a.ratings.length][];
-			Action[][] tempLowerActions = new Action[a.ratings.length][];
+	public RatedSchedule[] sort(RatedSchedule[] a){
+		RatedSchedule[] concatonatedSortedSchedules = new RatedSchedule[a.length];
+		if(a.length>1){
+			RatedSchedule pivot = a[0];
+			RatedSchedule[] tempHigher = new RatedSchedule[a.length];
+			RatedSchedule[] tempLower = new RatedSchedule[a.length];
 			
 			int higherIndex = 0;
 			int lowerIndex = 0;
-			for(int i = 1;i<a.ratings.length;i++){
-				if(a.ratings[i]>pivot){
-					tempHigher[higherIndex] = a.ratings[i];
-					tempHigherActions[higherIndex] = a.schedules[i];
+			for(int i = 1;i<a.length;i++){
+				if(a[i].rating>pivot.rating){
+					tempHigher[higherIndex] = a[i];
 					higherIndex++;
 				}else{
-					tempLower[lowerIndex] = a.ratings[i];
-					tempLowerActions[lowerIndex] = a.schedules[i];
+					tempLower[lowerIndex] = a[i];
 					lowerIndex++;
 				}
 			}
 			
-			double[] higher = new double[higherIndex];
-			double[] lower = new double[lowerIndex];
-			Action[][] higherActions = new Action[higherIndex][];
-			Action[][] lowerActions = new Action[lowerIndex][];
+			RatedSchedule[] higher = new RatedSchedule[higherIndex];
+			RatedSchedule[] lower = new RatedSchedule[lowerIndex];
 			for(int i = 0; i<higherIndex;i++){
 				higher[i] = tempHigher[i];
-				higherActions[i] = tempHigherActions[i];
 			}
 			for(int i = 0; i<lowerIndex; i++){
 				lower[i] = tempLower[i];
-				lowerActions[i] = tempLowerActions[i];
 			}
 			
-			RatedSchedules lowerSchedules = new RatedSchedules(lower, lowerActions);
-			RatedSchedules higherSchedules = new RatedSchedules(higher, higherActions);
+			RatedSchedule[] sortedHigher = sort(higher);
+			RatedSchedule[] sortedLower = sort(lower);
 			
-			higherSchedules = sort(higherSchedules);
-			lowerSchedules = sort(lowerSchedules);
+			System.arraycopy(sortedLower, 0, concatonatedSortedSchedules, 0, sortedLower.length);
+			concatonatedSortedSchedules[sortedLower.length] = pivot;
+			System.arraycopy(sortedHigher, 0, concatonatedSortedSchedules, sortedLower.length+1, sortedHigher.length);
 			
-			System.arraycopy(lowerSchedules.ratings, 0, sortedList, 0, lowerSchedules.ratings.length);
-			sortedList[lower.length] = pivot;
-			System.arraycopy(higherSchedules.ratings, 0, sortedList, lowerSchedules.ratings.length+1, higherSchedules.ratings.length);
-			
-			concatonatedSortedSchedules = new RatedSchedules(sortedList.length);
-			concatonatedSortedSchedules.ratings = sortedList;
-			
-			System.arraycopy(lowerSchedules.schedules, 0, concatonatedSortedSchedules.schedules, 0, lowerSchedules.schedules.length);
-			concatonatedSortedSchedules.schedules[lower.length] = pivotAction;
-			System.arraycopy(higherSchedules.schedules, 0, concatonatedSortedSchedules.schedules, lowerSchedules.schedules.length+1, higherSchedules.schedules.length);
-		
 		}else{
 			concatonatedSortedSchedules = a;
 		}
