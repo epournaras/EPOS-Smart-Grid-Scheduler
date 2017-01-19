912df6e7fa1d37fa7f2bc9fe395cd54b5b2716ed
STSWarren <stuartwarren1995@gmail.com>
2017-01-19 16:15:35 +0100

    Update ScheduleAndIndex.java

--- Current-Code/ScheduleAndIndex.java
+++ Current-Code/ScheduleAndIndex.java
@@ -1,9 +1,17 @@
 public class ScheduleAndIndex {
 	public int index;
 	public Action[] schedule;
+	public boolean finalPass;
 	
-	public ScheduleAndIndex(int i, Action[] s){
+	public ScheduleAndIndex(int i, Action[] s, boolean a){
 		this.index = i;
 		this.schedule = s;
+		this.finalPass = a;
+	}
+	
+	public ScheduleAndIndex(){
+		this.index = 0;
+		this.schedule = null;
+		this.finalPass = true;
 	}
 }
