4a72e2cbc9ef4809e6d28a739c4d55d789000ff5
STSWarren <stuartwarren1995@gmail.com>
2017-02-06 18:33:09 +0100

    Update Fragment3.java

--- Android-Code/Fragment3.java
+++ Android-Code/Fragment3.java
@@ -5,8 +5,11 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.application.R;
+
+import java.io.FileInputStream;
 
 public class Fragment3 extends Fragment {
 
@@ -16,7 +19,22 @@ public class Fragment3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment3, container, false);
+        View v = inflater.inflate(R.layout.fragment3, container, false);
+        Bundle args = getArguments();
+        String display = args.getString("displayToday");
+        try {
+            FileInputStream fis = getActivity().openFileInput("TodaySchedule.txt");
+            StringBuilder builder = new StringBuilder();
+            int ch;
+            while((ch = fis.read()) != -1){
+                builder.append((char)ch);
+            }
+            display = builder.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        TextView textView = (TextView)v.findViewById(R.id.text);
+        textView.setText(display);
+        return v;
     }
 }
