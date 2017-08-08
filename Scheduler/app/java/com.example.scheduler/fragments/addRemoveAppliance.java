5e31513662195c64e9d8dfb2e85459722859bf52
STSWarren <stuartwarren1995@gmail.com>
2017-08-08 17:35:16 +0200

    Create addRemoveAppliance.java

---
+++ Scheduler/app/java/com.example.scheduler/fragments/addRemoveAppliance.java
@@ -0,0 +1,171 @@
+package com.example.scheduler.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.example.scheduler.MainActivity;
+import com.example.scheduler.R;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by warrens on 07.08.17.
+ */
+
+public class addRemoveAppliance extends android.support.v4.app.DialogFragment {
+    private CheckBox[] applianceCheckBoxs;
+    private String[] applianceEnableBooleans;
+    private ArrayList checkBoxIdArrayList = new ArrayList();
+    private ArrayList relativeLayoutIdArrayList = new ArrayList();
+    private ArrayList<RelativeLayout> relativeLayoutArrayList = new ArrayList<>();
+    private String[] applianceNames;
+    @Override
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final Context context = getActivity();
+        final MainActivity m = (MainActivity)getActivity();
+        View v = inflater.inflate(R.layout.fragment_add_remove_appliance, container, false);
+        this.getDialog().setCanceledOnTouchOutside(true);
+        final String appliancesEnabledDataFile = "appliancesEnabledDataFile.txt";
+        String appliancesEnabled="";
+        try{
+            FileInputStream fis = getActivity().openFileInput(appliancesEnabledDataFile);
+            StringBuilder builder = new StringBuilder();
+            int chr;
+            while ((chr = fis.read()) != -1) {
+                builder.append((char) chr);
+            }
+            fis.close();
+            appliancesEnabled = builder.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        String[] appliancesEnabledArray = appliancesEnabled.split("\n");
+        final String[][] enableTable = new String[appliancesEnabledArray.length][2];
+        for(int i = 0 ; i<enableTable.length;i++){
+            enableTable[i] = appliancesEnabledArray[i].split(",");
+        }
+
+        String applianceNamesFile = "applianceNames.txt";
+        String appliancesNames="";
+        try{
+            FileInputStream fis = getActivity().openFileInput(applianceNamesFile);
+            StringBuilder builder = new StringBuilder();
+            int chr;
+            while ((chr = fis.read()) != -1) {
+                builder.append((char) chr);
+            }
+            fis.close();
+            appliancesNames = builder.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        applianceNames = appliancesNames.split(",");
+        applianceCheckBoxs = new CheckBox[applianceNames.length];
+        applianceEnableBooleans = new String[applianceNames.length];
+
+        RelativeLayout tempLayoutView;
+
+        LinearLayout lin = (LinearLayout)v.findViewById(R.id.checkBoxLayout);
+        for(int i = 0; i<applianceCheckBoxs.length;i++){
+            final int index = i;
+            final CheckBox temp;
+            tempLayoutView = new RelativeLayout(getActivity());
+            tempLayoutView.setId(v.generateViewId());
+            tempLayoutView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            temp = new CheckBox(getActivity());
+            temp.setId(v.generateViewId());
+            temp.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            temp.setText(applianceNames[i]);
+            if(applianceNames[i].equals(enableTable[i][0])){
+                applianceEnableBooleans[i] = "false";
+                if(enableTable[i][1].equals("true")){
+                    if(!temp.isChecked()){
+                        applianceEnableBooleans[i] = "true";
+                        temp.toggle();
+                    }
+                }else{
+                    if(temp.isChecked()){
+                        applianceEnableBooleans[i] = "false";
+                        temp.toggle();
+                    }
+                }
+            }
+            try{
+                lin.addView(tempLayoutView);
+                tempLayoutView.addView(temp);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            temp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View vi) {
+                    if(temp.isChecked()){
+                        applianceEnableBooleans[index] = "true";
+                    }else{
+                        applianceEnableBooleans[index] = "false";
+                    }
+                }
+            });
+            applianceCheckBoxs[i] = temp;
+            checkBoxIdArrayList.add(temp.getId());
+            relativeLayoutIdArrayList.add(tempLayoutView.getId());
+            relativeLayoutArrayList.add(tempLayoutView);
+        }
+        Button cancel = (Button) v.findViewById(R.id.button);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View g) {
+                dismiss();
+            }
+        });
+
+        Button confirm = (Button) v.findViewById(R.id.button2);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View g) {
+                String submitString = "";
+                for(int i = 0; i <applianceEnableBooleans.length;i++){
+                    if(enableTable.length == applianceEnableBooleans.length){
+                        enableTable[i][1] = applianceEnableBooleans[i];
+                    }
+                    for(int j = 0; j<enableTable[i].length;j++){
+                        if(j==enableTable[i].length-1){
+                            if(enableTable[i][j].equals("false")) m.removeItemWithName(enableTable[i][0]);
+                            submitString+=enableTable[i][j];
+                        }else{
+                            submitString+=enableTable[i][j]+",";
+                        }
+                    }
+                    submitString+="\n";
+                }
+                try{
+                    FileOutputStream fos = getActivity().openFileOutput(appliancesEnabledDataFile,Context.MODE_PRIVATE);
+                    fos.write(submitString.getBytes());
+                    fos.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                dismiss();
+            }
+        });
+        return v;
+    }
+}
